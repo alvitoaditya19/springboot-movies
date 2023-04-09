e80aa6c0ce81da351c5490e0daf4bc57531477c8
alvitoaditya19 <you@example.com>
2023-04-09 11:02:17 +0700

    update code

--- src/main/java/dev/alvito/movies/ReviewService.java
+++ src/main/java/dev/alvito/movies/ReviewService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
-public class ReviewServices {
+public class ReviewService {
 
     @Autowired
     private ReviewRepository repository;
@@ -22,7 +22,7 @@ public class ReviewServices {
 
         mongoTemplate.update(Movie.class)
             .matching(Criteria.where("imdbId").is(imdbId))
-            .apply(new Update().push("reviews").value(review))
+            .apply(new Update().push("reviewIds").value(review))
             .first();
 
         return review;
